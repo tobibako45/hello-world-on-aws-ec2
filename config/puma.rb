468c805be38bbdda64dc7c2204785e087955066d
tobibako45 <tobibako45@gmail.com>
2020-03-12 00:19:17 +0900

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -35,3 +35,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "unix:///var/www/hello-world-on-aws-ec2/tmp/sockets/hello-world-on-aws-ec2.sock"
